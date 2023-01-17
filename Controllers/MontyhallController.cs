00137847d7a2fe39ce7186ac2c0c3d33e3120969
IT21112300 <it21112300@my.sliit.lk>
2023-01-17 15:47:53 +0530

    Monty hall files

---
+++ Controllers/MontyhallController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MontyhallBackend.Models;
+using System;
+
+namespace MontyhallBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MontyhallController : ControllerBase
+    {
+
+        [HttpPost("Simulate")]
+        public IActionResult Simulate(Montyhall montyhall)
+        {
+            Random random = new Random();
+            int wins = 0;
+            int losses = 0;
+
+            // iterate our MontyHall routine
+            for (int i = 0; i < montyhall.SimulationCount; i++)
+            {
+
+                bool result = MontyHallPick(random.Next(3), montyhall.SwitchDoor, random.Next(3), random.Next(1));
+
+                if (result)
+                {
+                    wins++;
+                }
+                else
+                {
+                    losses++;
+                }
+            }
+
+            return Ok(new { wins });
+        }
+
+        public static bool MontyHallPick(int pickedDoor, int changeDoor, int carDoor, int goatDoorToRemove)
+        {
+            bool win = false;
+
+            // randomly remove one of the *goat* doors,
+            // but not the "contestants picked" ONE!
+            int leftGoat = 0;
+            int rightGoat = 2;
+            switch (pickedDoor)
+            {
+                case 0: leftGoat = 1; rightGoat = 2; break;
+                case 1: leftGoat = 0; rightGoat = 2; break;
+                case 2: leftGoat = 0; rightGoat = 1; break;
+            }
+
+            int keepGoat = goatDoorToRemove == 0 ? rightGoat : leftGoat;
+
+            // would the contestant win with the switch or the stay?
+            if (changeDoor == 0)
+            {
+                // not changing the initially picked door
+                win = carDoor == pickedDoor;
+            }
+            else
+            {
+                // changing picked door to the other door remaining
+                win = carDoor != keepGoat;
+            }
+
+            return win;
+        }
+
+    }
+
+}
+
---
+++ Models/Montyhall.cs
@@ -0,0 +1,8 @@
+﻿namespace MontyhallBackend.Models
+{
+    public class Montyhall
+    {
+        public int SimulationCount { get; set; }
+        public int SwitchDoor { get; set; }
+    }
+}
